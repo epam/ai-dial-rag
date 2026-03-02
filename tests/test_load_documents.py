0635b0f42efcdf4a7d6e5885f1a873411b2debdc
Aliaksei Labanau <aliaksei_labanau@epam.com>
2026-03-02 20:59:37 +0100

    fix: update libreoffice to 25.2.7 (#122)

--- tests/test_load_documents.py
+++ tests/test_load_documents.py
@@ -3,6 +3,7 @@ import sys
 import pytest
 
 from aidial_rag.attachment_link import AttachmentLink
+from aidial_rag.converter import convert_document_if_needed
 from aidial_rag.document_loaders import load_attachment, parse_document
 from tests.utils.local_http_server import start_local_server
 
@@ -19,8 +20,16 @@ async def load_document(name):
         display_name=name,
     )
 
-    file_metadata, buffer = await load_attachment(attachment_link, {})
-    mime_type = file_metadata.mime_type
+    file_metadata, original_file_bytes = await load_attachment(
+        attachment_link, {}
+    )
+
+    mime_type, buffer = await convert_document_if_needed(
+        mime_type=file_metadata.mime_type,
+        doc_bytes=original_file_bytes,
+        io_stream=sys.stderr,
+    )
+
     chunks = await parse_document(
         sys.stderr, buffer, mime_type, attachment_link, mime_type
     )
@@ -69,3 +78,18 @@ async def test_load_single_line_text(local_server):
     assert chunks[0].metadata["filetype"] == "text/plain"
     assert "page_number" not in chunks[0].metadata
     assert "orig_elements" not in chunks[0].metadata
+
+
+@pytest.mark.asyncio
+async def test_load_docx_with_long_table(local_server):
+    """Test that docx with long table can be loaded successfully.
+
+    Older versions of libreoffice may hang when converting this docx to pdf.
+    See https://ask.libreoffice.org/t/file-can-be-converted-to-pdf-using-gui-but-not-with-cmd/125926/3
+    """
+
+    chunks = await load_document("test_long_table.docx")
+    assert len(chunks) == 22
+    assert chunks[0].page_content.startswith("X")
+    assert chunks[0].metadata["filetype"] == "application/pdf"
+    assert chunks[0].metadata["page_number"] == 1
